c9e458cc80374c7cf984754af0163a50dd74261c
Alexey Shevchenko <kamikozz@mail.ru>
2022-03-28 16:04:45 +0300

    chore(cb-185): set production custom development email

--- src/appConstants/global.ts
+++ src/appConstants/global.ts
@@ -6,5 +6,5 @@ export const COMPLETE_MODAL_CONTRACT_CREATION_SUCCESS_TEXT = `${COMPLETE_MODAL_D
 
 export const isProduction = false;
 
-export const CUSTOM_DEVELOPMENT_EMAIL = isProduction ? 'c-dev@bitflex.app' : 'olga_verkhova@mywish.io' as const;
+export const CUSTOM_DEVELOPMENT_EMAIL = isProduction ? 'c-dev@bitflex.app' : 'c-dev@bitflex.app' as const;
 export const PRIVACY_SUPPORT_EMAIL = 'c-support@bitflex.app' as const;
