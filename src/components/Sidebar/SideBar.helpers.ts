67f366fcc3088dd9f1e99664044c6f2ff9018cf2
Alexey Shevchenko <kamikozz@mail.ru>
2022-03-28 16:10:04 +0300

    feat(cb-184): set mailto: link for Support section

--- src/components/Sidebar/SideBar.helpers.ts
+++ src/components/Sidebar/SideBar.helpers.ts
@@ -1,5 +1,5 @@
 import { FC } from 'react';
-import { routes } from 'appConstants';
+import { PRIVACY_SUPPORT_EMAIL, routes } from 'appConstants';
 import {
   FileTextIcon, PlusCircleIcon, SettingGearIcon, UsdIcon, AtIcon,
 } from 'theme/icons';
@@ -38,7 +38,7 @@ export const navigationTabs: INavigationTab[] = [
   {
     Icon: AtIcon,
     label: 'Support',
-    link: 'https://stackoverflow.com/questions/17492888/how-to-place-a-div-over-a-image',
+    link: `mailto:${PRIVACY_SUPPORT_EMAIL}`,
     isExternal: true,
   },
 ];
